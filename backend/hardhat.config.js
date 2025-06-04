0d438d9cf8b10f9b0e51c08ee50cdc03ec9e0047
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-06-03 21:20:47 -0300

    Refactor Hardhat configuration to use Sepolia network with environment variables

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,14 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config(); // 👈 Esto importa las
-
+require("dotenv").config(); // 👈 Esto importa las variables de entorno
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28", // Versión alineada con tu contrato
   networks: {
-    ephemery: {
-      url: "https://otter.bordel.wtf/erigon",
-      accounts: [process.env.PRIVATE_KEY],
+    sepolia: {
+      url: process.env.RPC_URL, // URL de Sepolia con Infura
+      accounts: [process.env.PRIVATE_KEY], // Llave privada desde las variables de entorno
     },
   },
 };
